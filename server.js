76791781cfb349781e2e86f1a9372d52a4bfb236
Luigi <dev.js.pad@gmail.com>
2020-08-12 17:28:08 +0200

    Add bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -30,6 +31,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+  bcrypt.compare('bacon', hash, function (err, res) {
+    // res == true
+  });
+  bcrypt.compare('veggies', hash, function (err, res) {
+    // res = false
+  });
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -81,6 +88,8 @@ app.post('/image', (req, res) => {
     res.status(400).json('not found');
   }
 });
+
+// Load hash from your password DB.
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
